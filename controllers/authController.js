9e119bfa213c9a78b08f870900fad2f88f64b7e4
“DanielCasallasG” <danielcasallas75@gmail.com>
2022-12-07 14:13:49 -0500

    Back

---
+++ controllers/authController.js
@@ -0,0 +1,45 @@
+const Usuario = require("../models/Usuarios");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path : "variables.env"});
+
+
+exports.autenticarUsuario = async (req, res) => {
+
+    const {password , email } = req.body;
+    try{
+        //revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({email});
+
+        if (!usuario){
+            return  res.status(404).json({ msg : "El usuario no existe "});
+        }
+        //Revisar password
+        const passwordCorrecto = await bcrypt.compare(password, usuario.password);
+        if(!passwordCorrecto){ 
+            return res.status(400).json({msg: "password incorrecto"})
+        }
+        // Si todo es correcto : crear y firmar un token
+        const payload = { 
+            usuario : {id: usuario.id},
+        };
+        //res.json(payload);
+        jwt.sign(
+            payload,
+            process.env.SECRETA,
+            {
+               expiresIn: "1d" //43200,//1 HORA
+            },
+            (error, token ) =>{
+                if (error) throw error;
+                //Mensaje de confirmacion
+                res.json({ token});
+            }
+        );
+
+        
+    }catch(error){
+        console.log(error);
+    }
+
+}
