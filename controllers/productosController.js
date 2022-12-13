9dc5b8b831fc03ffdfd65cb6b3067a5369701298
“DanielCasallasG” <danielcasallas75@gmail.com>
2022-12-12 20:18:38 -0500

    Back

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -3,50 +3,67 @@ const Usuario = require("../models/Productos");
 
 
 
-exports.obtenerProducto = async ( req, res) => {
-    try{
-        const producto = await Productos.find({ creador: req.usuario.id});
+exports.obtenerProducto = async (req, res) => {
+    try {
+        const Producto = await Productos.find({ creador: req.usuario.id });
 
         res.json({ producto });
-    }catch(error){
+    } catch (error) {
         console.log(error);
-    }};
+    }
+};
 
-exports.crearProducto = async ( req, res) => {
-    try{
+exports.crearProducto = async (req, res) => {
+    const { categoriaId } = req.body;
+    try {
+        const encontrarCategoria = await Categorias.findById(categoriaId);
+        console.log(encontrarCategoria);
+
+        const producto = new Productos(req.body);
+        producto.save();
+        res.json(producto);
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    /*try{
         const producto= new Productos(req.body);
         producto.creador =req.usuario.id;
         producto.save();
         res.json(producto);
     }catch(error){
         console.log(error);
-    };
-   
+    };*/
+
 };
 
-exports.actualizarProducto = async ( req, res) => {
+exports.actualizarProducto = async (req, res) => {
     const { id } = req.params;
     const producto = await Productos.findById(id);
 
-    if(!producto){
-        return res.status(400).json({ msg: "Producto no encontrado"});
+    if (!producto) {
+        return res.status(400).json({ msg: "Producto no encontrado" });
     }
-    if(producto.creador.toString() !== req.usuario.id.toString()){
-        return res.status(400).json({ msg : "acción no válida para este ususario"});
+    if (producto.creador.toString() !== req.usuario.id.toString()) {
+        return res.status(400).json({ msg: "acción no válida para este ususario" });
     }
 
     producto.nombre = req.body.nombre || producto.nombre;
 
     producto.save();
 
-    res.json({ producto});
+    res.json({ producto });
 };
 
-exports.borrarProducto = async ( req, res) => {
-    try{
-        await Productos.deleteOne({ _id: req.params.id});
-        res.json({ msg: "Producto eliminado"});
-    }catch(error){
+exports.borrarProducto = async (req, res) => {
+    try {
+        await Productos.deleteOne({ _id: req.params.id });
+        res.json({ msg: "Producto eliminado" });
+    } catch (error) {
         console.log(error);
     }
 };
